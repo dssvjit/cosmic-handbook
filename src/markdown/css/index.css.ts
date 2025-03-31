821b2be586a56f9c6ed69e8ffa71c2189309e8c3
https://github.com/Dharanilakkireddy <147068640+Dharanilakkireddy@users.noreply.github.com>
2025-03-31 14:03:06 +0530

    add css mdx

---
+++ src/markdown/css/index.css.ts
@@ -0,0 +1,13 @@
+import MD1 from "@/markdown/css/css.m1.mdx";
+import MD2 from "@/markdown/css/css.m2.mdx";
+import MD3 from "@/markdown/css/css.m3.mdx";
+import MD4 from "@/markdown/css/css.m4.mdx";
+import MD5 from "@/markdown/css/css.m5.mdx";
+
+export default {
+  MD1,
+  MD2,
+  MD3,
+  MD4,
+  MD5,
+};
