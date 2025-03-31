58a5fda881f4e8721da576ba8c3afb78bd917371
ohsoKool <rishikeshgunda9@gmail.com>
2025-03-31 17:21:31 +0530

    Add mdx for Prisma

--- src/components/handbook/markdown.handbook.tsx
+++ src/components/handbook/markdown.handbook.tsx
@@ -2,8 +2,7 @@ import MDXProviderWrapper from "@/components/mdx/mdx.provider";
 import { Button } from "@/components/ui/button";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import { useLocation } from "react-router-dom";
-import MD from "@/markdown/express/express.m6.mdx";
-
+import MD from "@/markdown/prisma/prisma.m1.mdx";
 const MarkdownHandbook = () => {
   const { pathname } = useLocation();
   console.log("PATHNAME: ", pathname);
