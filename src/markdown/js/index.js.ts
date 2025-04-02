895844a5c079213a6cd44611c902bf9a936c6a6c
saiusesgit <saisrujanpunati@gmail.com>
2025-04-02 20:17:21 +0530

    added index.js.ts file

---
+++ src/markdown/js/index.js.ts
@@ -0,0 +1,11 @@
+import MD1 from "@/markdown/js/js.m1.mdx";
+import MD2 from "@/markdown/js/js.m2.mdx";
+import MD3 from "@/markdown/js/js.m3.mdx";
+import MD4 from "@/markdown/js/js.m4.mdx";
+
+export default {
+  MD1,
+  MD2,
+  MD3,
+  MD4,
+};
