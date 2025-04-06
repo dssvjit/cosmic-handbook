9ec0889b1ebdcd3d10f56fe77b571ad1d7223beb
SneakyBuzZ <kaushikx304@gmail.com>
2025-04-06 12:11:59 +0530

    modify imports

--- src/markdown/nextjs/index.nextjs.ts
+++ src/markdown/nextjs/index.nextjs.ts
@@ -1,8 +1,8 @@
-import NextjsM1 from "./njs.m1.mdx";
-import NextjsM2 from "./njs.m2.mdx";
-import NextjsM3 from "./nextjs.m3.mdx";
-import NextjsM4 from "./nextjs.m4.mdx";
-import NextjsM5 from "./nextjs.m5.mdx";
+import NextjsM1 from "@/markdown/nextjs/nextjs.m1.mdx";
+import NextjsM2 from "@/markdown/nextjs/nextjs.m2.mdx";
+import NextjsM3 from "@/markdown/nextjs/nextjs.m3.mdx";
+import NextjsM4 from "@/markdown/nextjs/nextjs.m4.mdx";
+import NextjsM5 from "@/markdown/nextjs/nextjs.m5.mdx";
 
 export default {
   NextjsM1,
